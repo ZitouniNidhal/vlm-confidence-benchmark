eedae89e5c0b282a617bb41ad8a2d067ee067c77
ZITOUNI Nidhal <nidhalzitouni111@gmail.com>
2026-07-30 17:25:23 +0100

    Add apply_resample function for image resizing

---
+++ degradations/resample.py
@@ -0,0 +1,4 @@
+def apply_resample(image, scale: float = 0.5):
+    width, height = image.size
+    new_size = (max(1, int(width * scale)), max(1, int(height * scale)))
+    return image.resize(new_size, resample=Image.BICUBIC).resize((width, height), resample=Image.BICUBIC)
